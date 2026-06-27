9a9323c3b8fb9c127fee170f39ade2c8ce41bb09
VANSH-73-WEB <v333777abc@gmail.com>
2026-06-27 16:14:11 +0530

    done

---
+++ voting-contract/test/Voting.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Voting Contract", function () {
+
+  async function deployContract() {
+    const Voting = await ethers.getContractFactory("Voting");
+
+    const voting = await Voting.deploy();
+    await voting.waitForDeployment();
+
+    return voting;
+  }
+
+  it("Should deploy successfully", async function () {
+    const voting = await deployContract();
+
+    expect(await voting.getAddress()).to.not.equal(
+      ethers.ZeroAddress
+    );
+  });
+
+});
